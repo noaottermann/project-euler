e3d753856c2067ef4aa1efe2ec1cd7bd472c2862
NoaLeGeek <noacraft68@gmail.com>
2026-03-15 19:24:33 +0100

    Implement solution for Euler Problem 9

--- p009.py
+++ p009.py
@@ -4,7 +4,11 @@ from utils import *
 # Euler Problem 9
 
 def solve():
-    pass # TODO: Implement the solution for Problem 9
+    for a in range(1, 1000):
+        for b in range(a, 1000 - a):
+            c = 1000 - a - b
+            if a**2 + b**2 == c**2:
+                return a * b * c
 
 if __name__ == '__main__':
     run(solve, problem_id=9)
