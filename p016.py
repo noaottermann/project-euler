53f5976b0f12b4f755bc3b132c80be3f3eb3f9d9
NoaLeGeek <noacraft68@gmail.com>
2026-03-15 21:24:59 +0100

    Implement solution for Euler Problem 16

--- p016.py
+++ p016.py
@@ -4,7 +4,7 @@ from utils import *
 # Euler Problem 16
 
 def solve():
-    pass # TODO: Implement the solution for Problem 16
+    return sum(int(digit) for digit in str(2**1000))
 
 if __name__ == '__main__':
     run(solve, problem_id=16)
