a89ff4ea4d9806f7157b5c38566fc9a1b43b538c
NoaLeGeek <noacraft68@gmail.com>
2026-03-15 19:02:15 +0100

    Implement solution for Euler Problem 3

--- p003.py
+++ p003.py
@@ -4,7 +4,14 @@ from utils import *
 # Euler Problem 3
 
 def solve():
-    pass # TODO: Implement the solution for Problem 3
+    n = 600851475143
+    factor = 2
+    while factor * factor <= n:
+        if n % factor == 0:
+            n //= factor
+        else:
+            factor += 1
+    return n
 
 if __name__ == '__main__':
     run(solve, problem_id=3)
