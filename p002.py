3b72088cfcdb238c8ec282393621b2b5799e514a
NoaLeGeek <noacraft68@gmail.com>
2026-03-15 19:01:23 +0100

    Implement solution for Euler Problem 2

--- p002.py
+++ p002.py
@@ -4,7 +4,13 @@ from utils import *
 # Euler Problem 2
 
 def solve():
-    pass # TODO: Implement the solution for Problem 2
+    a, b = 1, 2
+    total = 0
+    while a <= 4*10**6:
+        if a % 2 == 0:
+            total += a
+        a, b = b, a + b
+    return total
 
 if __name__ == '__main__':
     run(solve, problem_id=2)
