14410853a5bd5ec09672c1cad7e19885cd9ced8d
NoaLeGeek <noacraft68@gmail.com>
2026-03-15 19:18:53 +0100

    Implement solution for Euler Problem 7

--- p007.py
+++ p007.py
@@ -4,7 +4,14 @@ from utils import *
 # Euler Problem 7
 
 def solve():
-    pass # TODO: Implement the solution for Problem 7
+    n = 10001
+    count = 0
+    num = 1
+    while count < n:
+        num += 1
+        if is_prime(num):
+            count += 1
+    return num
 
 if __name__ == '__main__':
     run(solve, problem_id=7)
