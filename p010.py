16fd9a7edb4db11c190ea3524f75028119ad66a5
NoaLeGeek <noacraft68@gmail.com>
2026-03-15 19:28:57 +0100

    Implement solution for Euler Problem 10

--- p010.py
+++ p010.py
@@ -4,7 +4,14 @@ from utils import *
 # Euler Problem 10
 
 def solve():
-    pass # TODO: Implement the solution for Problem 10
+    limit = 2*10**6
+    sieve = [True] * limit
+    sieve[0] = sieve[1] = False
+    for i in range(2, int(limit**0.5) + 1):
+        if sieve[i]:
+            for j in range(i * i, limit, i):
+                sieve[j] = False
+    return sum(i for i in range(limit) if sieve[i])
 
 if __name__ == '__main__':
     run(solve, problem_id=10)
