d0eb84d016dd28640ca45381a0478c057ead6488
NoaLeGeek <noacraft68@gmail.com>
2026-03-15 21:30:29 +0100

    Implement solution for Euler Problem 19

--- p019.py
+++ p019.py
@@ -1,10 +1,16 @@
 from euler import run
 from utils import *
+from datetime import datetime
 
 # Euler Problem 19
 
 def solve():
-    pass # TODO: Implement the solution for Problem 19
+    count = 0
+    for year in range(1901, 2001):
+        for month in range(1, 13):
+            if datetime(year, month, 1).weekday() == 6:
+                count += 1
+    return count
 
 if __name__ == '__main__':
     run(solve, problem_id=19)
