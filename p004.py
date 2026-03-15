1856d468097d8887e2820ac95dbf41e18b73d051
NoaLeGeek <noacraft68@gmail.com>
2026-03-15 19:05:21 +0100

    Implement solution for Euler Problem 4

--- p004.py
+++ p004.py
@@ -4,7 +4,13 @@ from utils import *
 # Euler Problem 4
 
 def solve():
-    pass # TODO: Implement the solution for Problem 4
+    max_palindrome = 0
+    for i in range(100, 1000):
+        for j in range(i, 1000):
+            product = i * j
+            if product > max_palindrome and is_palindrome(product):
+                max_palindrome = product
+    return max_palindrome
 
 if __name__ == '__main__':
     run(solve, problem_id=4)
