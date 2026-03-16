9cef5b345bbeb2f6de7449462d9d1f3d2be05628
Noa OTTERMANN <noaottermann68@gmail.com>
2026-03-16 17:33:51 +0100

    Implement solution for Euler Problem 28

--- p028.py
+++ p028.py
@@ -4,7 +4,14 @@ from utils import *
 # Euler Problem 28
 
 def solve():
-    pass # TODO: Implement the solution for Problem 28
+    total = 1
+    n = 1
+    for i in range(1, 1001, 2):
+        for _ in range(4):
+            n += i
+            total += n
+    return total
+
 
 if __name__ == '__main__':
     run(solve, problem_id=28)
