bedb2921b58778c129ef0cb08ebee169d7d5d0a3
NoaLeGeek <noacraft68@gmail.com>
2026-03-16 21:58:23 +0100

    Implement solution for Euler Problem 40

--- p040.py
+++ p040.py
@@ -4,7 +4,11 @@ from utils import *
 # Euler Problem 40
 
 def solve():
-    pass # TODO: Implement the solution for Problem 40
+    concatenated = ''.join(str(i) for i in range(1, 1000000))
+    product = 1
+    for i in range(7):
+        product *= int(concatenated[10**i - 1])
+    return product
 
 if __name__ == '__main__':
     run(solve, problem_id=40)
