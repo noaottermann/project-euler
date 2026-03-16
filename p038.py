c1fb6d9db681e30cc9e6337210aeda0c39d4c29e
NoaLeGeek <noacraft68@gmail.com>
2026-03-16 21:32:23 +0100

    Refactor solution for Euler Problem 38 to return the maximum pandigital number instead of the first valid one

--- p038.py
+++ p038.py
@@ -6,11 +6,13 @@ from utils import *
 def solve():
     # n > 1 because we need at least two multiples to concatenate
     # 10**(9 // n) is the largest integer i such that the concatenation of i, 2*i, ..., n*i has at most 9 digits
+    maximum_pandigital = 0
     for n in range(2, 10):
         for i in range(1, 10**(9 // n)):
             pandigital = ''.join(str(i * j) for j in range(1, n + 1))
             if len(pandigital) == 9 and set(pandigital) == set('123456789'):
-                return int(pandigital)
+                maximum_pandigital = max(maximum_pandigital, int(pandigital))
+    return maximum_pandigital
             
 if __name__ == '__main__':
     run(solve, problem_id=38)
