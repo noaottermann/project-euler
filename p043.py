a93b8f17fe86440e6e355871605de569b3e50e5d
Noa OTTERMANN <noaottermann68@gmail.com>
2026-03-17 14:33:32 +0100

    Implement solution for Euler Problem 43

--- p043.py
+++ p043.py
@@ -4,7 +4,19 @@ from utils import *
 # Euler Problem 43
 
 def solve():
-    pass # TODO: Implement the solution for Problem 43
+    pandigital_numbers = generate_permutations("".join(str(i) for i in range(0, 10)))
+    total = 0
+    for num in pandigital_numbers:
+        if (int(num[1:4]) % 2 == 0 and
+            int(num[2:5]) % 3 == 0 and
+            int(num[3:6]) % 5 == 0 and
+            int(num[4:7]) % 7 == 0 and
+            int(num[5:8]) % 11 == 0 and
+            int(num[6:9]) % 13 == 0 and
+            int(num[7:10]) % 17 == 0):
+            total += int(num)
+    return total
+
 
 if __name__ == '__main__':
     run(solve, problem_id=43)
