d785430e9f4500d0ae79db91401b2d402688b8a5
NoaLeGeek <noacraft68@gmail.com>
2026-03-17 19:30:01 +0100

    Implement solution for Euler Problem 44

--- p044.py
+++ p044.py
@@ -4,7 +4,17 @@ from utils import *
 # Euler Problem 44
 
 def solve():
-    pass # TODO: Implement the solution for Problem 44
+    # 10000 is 
+    pentagonal_numbers = set()
+    for n in range(1, 10000):
+        pentagonal_number = n * (3 * n - 1) // 2
+        pentagonal_numbers.add(pentagonal_number)
+    min_diff = float('inf')
+    for p1 in pentagonal_numbers:
+        for p2 in pentagonal_numbers:
+            if p1 != p2 and (p1 + p2) in pentagonal_numbers and abs(p1 - p2) in pentagonal_numbers:
+                min_diff = min(min_diff, abs(p1 - p2))
+    return min_diff
 
 if __name__ == '__main__':
     run(solve, problem_id=44)
