f2a241dd475a1adecaac9aeae0fbc4c55cb6d04c
Noa OTTERMANN <noaottermann68@gmail.com>
2026-03-18 17:29:54 +0100

    Implement solution for Euler Problem 47 and add distinct_prime_factors utility function

--- p047.py
+++ p047.py
@@ -4,7 +4,21 @@ from utils import *
 # Euler Problem 47
 
 def solve():
-    pass # TODO: Implement the solution for Problem 47
+    # 210 is the smallest number that can be expressed as the product of 4 distinct primes
+    # We need to optimize the search by remembering numbers that doesn't have 4 distinct prime factors so we can skip them
+    n = 2*3*5*7
+    invalid = set()
+    while True:
+        for i in range(n, n+4):
+            if i in invalid:
+                n = i + 1
+                break
+            if distinct_prime_factors(i) != 4:
+                invalid.add(i)
+                n = i + 1
+                break
+        else:
+            return n
 
 if __name__ == '__main__':
     run(solve, problem_id=47)
--- utils.py
+++ utils.py
@@ -444,3 +444,14 @@ def is_fibonacci(n):
 
 def is_composite(n):
     return n > 1 and not is_prime(n)
+
+def distinct_prime_factors(n):
+    count = 0
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            count += 1
+            while n % i == 0:
+                n //= i
+    if n > 1:
+        count += 1
+    return count
