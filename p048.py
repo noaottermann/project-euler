7722bd3b9805e362e5e02f3f0398ff22a718eac0
Noa OTTERMANN <noaottermann68@gmail.com>
2026-03-18 17:32:29 +0100

    Implement solution for Euler Problem 48

--- p048.py
+++ p048.py
@@ -4,7 +4,7 @@ from utils import *
 # Euler Problem 48
 
 def solve():
-    pass # TODO: Implement the solution for Problem 48
+    return str(sum(i**i for i in range(1, 1001)))[-10:]
 
 if __name__ == '__main__':
     run(solve, problem_id=48)
