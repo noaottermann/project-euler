e94a169def7ae835f423e1dc0d729ee64f83069f
Noa OTTERMANN <noaottermann68@gmail.com>
2026-03-18 19:46:30 +0100

    Implement solution for Euler Problem 49

--- p049.py
+++ p049.py
@@ -4,7 +4,27 @@ from utils import *
 # Euler Problem 49
 
 def solve():
-    pass # TODO: Implement the solution for Problem 49
+    primes = [n for n in range(1000, 10000) if is_prime(n)]
+    
+    prime_groups = {}
+    for prime in primes:
+        key = ''.join(sorted(str(prime)))
+        if key == "1478": # This is the known sequence, we want to skip it
+            continue
+        if key not in prime_groups:
+            prime_groups[key] = []
+        prime_groups[key].append(prime)
+    
+    for group in prime_groups.values():
+        if len(group) < 3:
+            continue
+        group.sort()
+        for i in range(len(group)):
+            for j in range(i + 1, len(group)):
+                a, b = group[i], group[j]
+                c = b + (b - a)
+                if c in group:
+                    return str(a) + str(b) + str(c)
 
 if __name__ == '__main__':
     run(solve, problem_id=49)
