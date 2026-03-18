a89ff448848559c5ecd78240967b512d48d801ad
Noa OTTERMANN <noaottermann68@gmail.com>
2026-03-18 20:10:50 +0100

    Implement solution for Euler Problem 50

--- p050.py
+++ p050.py
@@ -4,7 +4,22 @@ from utils import *
 # Euler Problem 50
 
 def solve():
-    pass # TODO: Implement the solution for Problem 50
+    limit = 10**6
+    primes = list(sieve_of_eratosthenes(limit))
+
+    longest_length = 0
+    longest_prime = 0
+
+    for i in range(len(primes)):
+        for j in range(i + longest_length, len(primes)):
+            s = sum(primes[i:j])
+            if s >= limit:
+                break
+            if s in primes:
+                longest_length = j - i
+                longest_prime = s
+
+    return longest_prime
 
 if __name__ == '__main__':
     run(solve, problem_id=50)
