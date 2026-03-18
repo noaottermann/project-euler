e4318f1414fc247550c84c6124443c766c5078a4
Noa OTTERMANN <noaottermann68@gmail.com>
2026-03-18 16:39:19 +0100

    Implement solution for Euler Problem 45

--- p045.py
+++ p045.py
@@ -3,8 +3,14 @@ from utils import *
 
 # Euler Problem 45
 
-def solve():
-    pass # TODO: Implement the solution for Problem 45
+def solve(): 
+    # n = 143 is the first triangle number that is also pentagonal and hexagonal, so we start from there
+    n = 144
+    while True:
+        n += 1
+        hexagonal_number = n * (2 * n - 1)
+        if is_pentagonal(hexagonal_number) and is_triangular(hexagonal_number):
+            return hexagonal_number
 
 if __name__ == '__main__':
     run(solve, problem_id=45)
