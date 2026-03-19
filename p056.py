60ed06654d2e83ba015b8300d7d9e26378db9c03
Noa OTTERMANN <noaottermann68@gmail.com>
2026-03-19 20:18:55 +0100

    Implement solution for Euler Problem 56

--- p056.py
+++ p056.py
@@ -4,7 +4,7 @@ from utils import *
 # Euler Problem 56
 
 def solve():
-    pass # TODO: Implement the solution for Problem 56
+    return max(sum_digits(a**b) for a in range(100) for b in range(100))
 
 if __name__ == '__main__':
     run(solve, problem_id=56)
