9f2b16d6dd1aabe0195dcee064f6e2096f0e1a21
Noa OTTERMANN <noaottermann68@gmail.com>
2026-03-19 20:12:22 +0100

    Implement solution for Euler Problem 54 Add external files for big data texts

--- p018.py
+++ p018.py
@@ -4,22 +4,7 @@ from utils import *
 # Euler Problem 18
 
 def solve():
-    triangle = """75
-            95 64
-            17 47 82
-            18 35 87 10
-            20 04 82 47 65
-            19 01 23 75 03 34
-            88 02 77 73 07 63 67
-            99 65 04 28 06 16 70 92
-            41 41 26 56 83 40 80 70 33
-            41 48 72 33 47 32 37 16 94 29
-            53 71 44 65 25 43 91 52 97 51 14
-            70 11 33 28 77 73 17 78 39 68 17 57
-            91 71 52 38 17 14 91 43 58 50 27 29 48
-            63 66 04 68 89 53 67 30 73 16 69 87 40 31
-            04 62 98 27 23 09 70 98 73 93 38 53 60 04 23"""
-    triangle = [list(map(int, row.split())) for row in triangle.splitlines()]
+    triangle = [list(map(int, line.strip().split())) for line in open('p018_triangle.txt')]
 
     for row in range(len(triangle) -2,-1,-1):
         for col in range(len(triangle[row])):
--- p054.py
+++ p054.py
@@ -4,7 +4,88 @@ from utils import *
 # Euler Problem 54
 
 def solve():
-    pass # TODO: Implement the solution for Problem 54
+    poker = [line.strip() for line in open('p054_poker.txt')]
+    player1_wins = 0
+    for hand in poker:
+        cards = hand.split()
+        player1 = cards[:5]
+        player2 = cards[5:]
+        if compare_hands(player1, player2) > 0:
+            player1_wins += 1
+    return player1_wins
+    
+def compare_hands(hand1, hand2):
+    score1 = hand_rank(hand1)
+    score2 = hand_rank(hand2)
+    if score1 > score2:
+        return 1
+    if score1 < score2:
+        return -1
+    return 0
+    
+def hand_rank(hand):
+    values = sorted([card_value(card) for card in hand], reverse=True)
+    suits = [card[1] for card in hand]
+    counts = {}
+    for v in values:
+        counts[v] = counts.get(v, 0) + 1
+    groups = sorted(((count, value) for value, count in counts.items()), reverse=True)
 
+    is_flush = len(set(suits)) == 1
+
+    unique_values = sorted(set(values), reverse=True)
+    is_straight = False
+    straight_high = None
+    if len(unique_values) == 5:
+        if unique_values[0] - unique_values[-1] == 4:
+            is_straight = True
+            straight_high = unique_values[0]
+        elif unique_values == [14, 5, 4, 3, 2]:
+            is_straight = True
+            straight_high = 5
+
+    if is_straight and is_flush and straight_high == 14:
+        return (10, [14])  # Royal Flush
+    if is_straight and is_flush:
+        return (9, [straight_high])  # Straight Flush
+    if groups[0][0] == 4:
+        four_value = groups[0][1]
+        kicker = groups[1][1]
+        return (8, [four_value, kicker])  # Four of a Kind
+    if groups[0][0] == 3 and groups[1][0] == 2:
+        return (7, [groups[0][1], groups[1][1]])  # Full House
+    if is_flush:
+        return (6, values)  # Flush
+    if is_straight:
+        return (5, [straight_high])  # Straight
+    if groups[0][0] == 3:
+        trip_value = groups[0][1]
+        kickers = sorted([v for v in values if v != trip_value], reverse=True)
+        return (4, [trip_value] + kickers)  # Three of a Kind
+    if groups[0][0] == 2 and groups[1][0] == 2:
+        pair_values = sorted([groups[0][1], groups[1][1]], reverse=True)
+        kicker = groups[2][1]
+        return (3, pair_values + [kicker])  # Two Pairs
+    if groups[0][0] == 2:
+        pair_value = groups[0][1]
+        kickers = sorted([v for v in values if v != pair_value], reverse=True)
+        return (2, [pair_value] + kickers)  # One Pair
+    return (1, values)  # High Card
+    
+def card_value(card):
+    value_str = card[0]
+    if value_str.isdigit():
+        return int(value_str)
+    elif value_str == 'T':
+        return 10
+    elif value_str == 'J':
+        return 11
+    elif value_str == 'Q':
+        return 12
+    elif value_str == 'K':
+        return 13
+    elif value_str == 'A':
+        return 14
+    
 if __name__ == '__main__':
     run(solve, problem_id=54)
