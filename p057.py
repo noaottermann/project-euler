a0f7797322dacf776e792a71e8a787cf7f60ec46
Noa OTTERMANN <noaottermann68@gmail.com>
2026-03-20 16:18:58 +0100

    Implement solution for Euler Problem 57

--- p057.py
+++ p057.py
@@ -4,7 +4,18 @@ from utils import *
 # Euler Problem 57
 
 def solve():
-    pass # TODO: Implement the solution for Problem 57
+    # a is the numerator, b is the denominator
+    # Explanation:
+    # The nth expansion can be found by the following recurrence:
+    # 1 + 1/(1 + a(n)/b(n)) = 1 + b(n)/(a(n) + b(n)) = (a(n) + 2*b(n))/(a(n) + b(n))
+    # So a(n+1) = a(n) + 2*b(n) and b(n+1) = a(n) + b(n)
+    count = 0
+    a, b = 1, 1
+    for _ in range(1000):
+        a, b = a + 2 * b, a + b
+        if len(str(a)) > len(str(b)):
+            count += 1
+    return count
 
 if __name__ == '__main__':
     run(solve, problem_id=57)
