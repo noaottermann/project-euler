6bf930e61010f3367c0851e2acfb81625ddcb459
NoaLeGeek <noacraft68@gmail.com>
2026-03-20 22:04:39 +0100

    Implement solution for Euler Problem 97

--- p097.py
+++ p097.py
@@ -4,7 +4,10 @@ from utils import *
 # Euler Problem 97
 
 def solve():
-    pass # TODO: Implement the solution for Problem 97
+    mod = 10**10
+    power = pow(2, 7830457, mod)
+    result = (28433 * power + 1) % mod
+    return result
 
 if __name__ == '__main__':
     run(solve, problem_id=97)
