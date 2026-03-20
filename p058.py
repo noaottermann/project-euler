dfadf73d07fb01de611d4c67f599dabb49216e2c
Noa OTTERMANN <noaottermann68@gmail.com>
2026-03-20 16:42:54 +0100

    Implement solution for Euler Problem 58

--- p058.py
+++ p058.py
@@ -4,7 +4,17 @@ from utils import *
 # Euler Problem 58
 
 def solve():
-    pass # TODO: Implement the solution for Problem 58
+    n = 1
+    total = 1
+    primes = 0
+    while True:
+        n += 2
+        total += 4
+        for i in range(4):
+            if is_prime(n**2 - i*(n-1)):
+                primes += 1
+        if primes / total < 0.10:
+            return n
 
 if __name__ == '__main__':
     run(solve, problem_id=58)
