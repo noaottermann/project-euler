6b88981dc30d03f0bf1abeef89413f0a09f5d534
NoaLeGeek <noacraft68@gmail.com>
2026-03-27 14:39:45 +0100

    Implement solution for Euler Problem 63

--- p063.py
+++ p063.py
@@ -4,7 +4,18 @@ from utils import *
 # Euler Problem 63
 
 def solve():
-    pass # TODO: Implement the solution for Problem 63
+    total = 0
+    n = 1
+    while True:
+        count = 0
+        for p in range(1, 10):
+            if len(str(p**n)) == n:
+                count += 1
+        if count == 0:
+            break
+        total += count
+        n += 1
+    return total
 
 if __name__ == '__main__':
     run(solve, problem_id=63)
