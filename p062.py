7070c9993cf3bbb19258c98898ce23cb48b2a84c
NoaLeGeek <noacraft68@gmail.com>
2026-03-27 12:14:16 +0100

    Implement solution for Euler Problem 62

--- p062.py
+++ p062.py
@@ -4,7 +4,15 @@ from utils import *
 # Euler Problem 62
 
 def solve():
-    pass # TODO: Implement the solution for Problem 62
+    cubes = {}
+    n = 1
+    while True:
+        c = n**3
+        key = ''.join(sorted(str(c)))
+        cubes.setdefault(key, []).append(c)
+        if len(cubes[key]) == 5:
+            return min(cubes[key])
+        n += 1
 
 if __name__ == '__main__':
     run(solve, problem_id=62)
