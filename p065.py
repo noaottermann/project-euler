9583a74e17ce8659e9da5e76d161c464fa1cc1b4
NoaLeGeek <noacraft68@gmail.com>
2026-03-29 19:10:18 +0200

    Implement solution for Euler Problem 65

--- p065.py
+++ p065.py
@@ -4,7 +4,13 @@ from utils import *
 # Euler Problem 65
 
 def solve():
-    pass # TODO: Implement the solution for Problem 65
+    # The nth term of the sequence is given by a(n) = 1 if n % 3 != 0 else 2 * (n // 3) for n >= 2
+    terms = [2] + [1 if n % 3 != 0 else 2 * (n // 3) for n in range(2, 101)]
+    numerator = 1
+    denominator = terms[-1]
+    for term in terms[:-1][::-1]:
+        numerator, denominator = denominator, term * denominator + numerator
+    return sum(int(digit) for digit in str(denominator))
 
 if __name__ == '__main__':
     run(solve, problem_id=65)
