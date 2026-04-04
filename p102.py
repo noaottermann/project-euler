d18e1b4f8504ac8fa75a77d78e6ea3a44d83ee82
Noa OTTERMANN <noaottermann68@gmail.com>
2026-04-04 20:41:37 +0200

    Implement solution for Euler Problem 102

--- p102.py
+++ p102.py
@@ -4,7 +4,19 @@ from utils import *
 # Euler Problem 102
 
 def solve():
-    pass # TODO: Implement the solution for Problem 102
+    count = 0
+    with open("p102_triangles.txt", "r") as f:
+        for line in f:
+            x1, y1, x2, y2, x3, y3 = map(int, line.strip().split(","))
+            area = abs(x1*(y2-y3) + x2*(y3-y1) + x3*(y1-y2)) / 2
+            if area > 0:
+                area1 = abs(0*(y2-y3) + x2*(y3-0) + x3*(0-y2)) / 2
+                area2 = abs(x1*(0-y3) + 0*(y3-y1) + x3*(y1-0)) / 2
+                area3 = abs(x1*(y2-0) + x2*(0-y1) + 0*(y1-y2)) / 2
+                if area == area1 + area2 + area3:
+                    count += 1
+    return count
+
 
 if __name__ == '__main__':
     run(solve, problem_id=102)
